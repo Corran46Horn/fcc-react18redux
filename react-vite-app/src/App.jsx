25c77feebec6f06b1bfd32ecf0837ff40823e447
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-23 16:44:25 +0100

    useState

--- react-vite-app/src/App.jsx
+++ react-vite-app/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
 import Starter from './tutorial/01-useState/starter/01-error-example';
-import Final from './tutorial/01-useState/final/01-error-example';
+//import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
   return (
     <div className='container'>
       <Starter />
-      <Final />
     </div>
   );
 }
--- react-vite-app/src/tutorial/01-useState/starter/01-error-example.jsx
+++ react-vite-app/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,35 @@
+import { data } from '../../../data';
+import { useState } from 'react';
+
 const ErrorExample = () => {
-  return <h2>useState not so bad example</h2>;
+  let [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    console.log(id);
+    const newList = people.filter((person) => person.id !== id);
+    setPeople(newList);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type='button' onClick={() => removeItem(id)}>
+              usuń mnie
+            </button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={clearAllItems}>
+        usuń nas
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
