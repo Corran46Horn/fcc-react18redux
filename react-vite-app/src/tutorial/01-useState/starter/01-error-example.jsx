10276fe78da1dd5480f0f90f86f1763b47071809
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-23 16:45:45 +0100

    poprawna kolejnosc

--- react-vite-app/src/tutorial/01-useState/starter/01-error-example.jsx
+++ react-vite-app/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,32 +1,14 @@
-import { data } from '../../../data';
 import { useState } from 'react';
-
 const ErrorExample = () => {
-  let [people, setPeople] = useState(data);
-
-  const removeItem = (id) => {
-    console.log(id);
-    const newList = people.filter((person) => person.id !== id);
-    setPeople(newList);
-  };
-  const clearAllItems = () => {
-    setPeople([]);
+  let [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
   };
   return (
     <div>
-      {people.map((person) => {
-        const { id, name } = person;
-        return (
-          <div key={id}>
-            <h4>{name}</h4>
-            <button type='button' onClick={() => removeItem(id)}>
-              usuń mnie
-            </button>
-          </div>
-        );
-      })}
-      <button className='btn' onClick={clearAllItems}>
-        usuń nas
+      <h2 style={{ textTransform: 'lowercase' }}>kliknąłeś {count} razy</h2>
+      <button type='button' className='btn' onClick={increase}>
+        +
       </button>
     </div>
   );
