ec4ccca46265e83f6694d231bf099e3def6cf90c
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-23 17:56:36 +0100

    useState removing items

--- react-vite-app/src/App.jsx
+++ react-vite-app/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Starter from './tutorial/01-useState/starter/01-error-example';
+import Starter from './tutorial/01-useState/starter/03-useState-array';
 //import Final from './tutorial/01-useState/final/03-useState-array';
 
 function App() {
--- react-vite-app/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ react-vite-app/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,38 @@
+import { data } from '../../../data';
+import { useState } from 'react';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const deleteAll = () => {
+    setPeople([]);
+  };
+  const deleteMe = (id) => {
+    console.log(id);
+    const newList = people.filter((person) => person.id !== id);
+    setPeople(newList);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button
+              onClick={() => {
+                deleteMe(id);
+              }}
+            >
+              usuń mnie
+            </button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={deleteAll}>
+        Usuń nas
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
