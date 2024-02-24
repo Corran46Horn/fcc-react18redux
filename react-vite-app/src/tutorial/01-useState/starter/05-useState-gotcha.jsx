2f09524ffc5f927cf7c3c440b1cc07167177cf73
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-24 01:38:04 +0100

    useTimeout usestate

--- react-vite-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ react-vite-app/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  let [num, setNum] = useState(0);
+  //update state zgodnie z logiem
+  /*const updateButton = () => {
+    setNum((currentState) => {
+      const newState = currentState + 1;
+      return newState;
+      console.log(newState);
+    });
+    console.log(num);
+  };*/
+  //wszystkie naciśnięcia policzone
+  const updateButton = () => {
+    // setTimeout(() => {
+    // console.log('clicked the button');
+    //   setValue(value + 1);
+    // }, 3000);
+    setTimeout(() => {
+      console.log('clicked the button');
+      setNum((currentState) => {
+        return currentState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <div>
+      <h2>{num}</h2>
+      <button type='button' className='btn' onClick={updateButton}>
+        Update
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
