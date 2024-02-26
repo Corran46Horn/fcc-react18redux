b86bb7a8ca89bf348cd3369ef2dfd3e1ecb31b84
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-26 15:47:33 +0100

    fetching data useffect

--- react-vite-app/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ react-vite-app/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -5,6 +5,31 @@ const url = 'https://api.github.com/users';
 const FetchData = () => {
   const [users, setUsers] = useState([]);
   //useEffect FECZ
-  return <h2>{users.name}</h2>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <ul className='users'>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <li key={id}>
+            <h3>{login}</h3>
+            <img src={avatar_url} alt={login} />
+            <a href={html_url}>profile</a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 export default FetchData;
