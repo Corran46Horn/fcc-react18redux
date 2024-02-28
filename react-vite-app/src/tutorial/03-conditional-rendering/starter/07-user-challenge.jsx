0602e7997849d557f76f3696cd20221d979badb6
Kornel Wiśniewski <kor.wisniewski@gmail.com>
2024-02-28 21:07:32 +0100

    user challenge

--- react-vite-app/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ react-vite-app/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const logIn = () => {
+    setUser({ name: 'Anna' });
+  };
+  const logOut = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there {user.name}</h4>
+          <button className='btn' onClick={logOut}>
+            Log out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please login</h4>
+          <button className='btn' onClick={logIn}>
+            Log in
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
